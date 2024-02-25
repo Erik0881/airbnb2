26837ac8885e3b91d8399129ef550626f98e5272
Erkin Yusupov（Primary School） <erkinyusupov@ECNUXP.COM>
2024-02-25 12:15:59 +0800

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "glvmmupiqwlmhicmggqp.supabase.co",
+        protocol: "https",
+        port: "",
+      },
+      {
+        hostname: "a0.muscache.com",
+        protocol: "https",
+        port: "",
+      },
+      {
+        hostname: "osyucybbknugwtlebzfw.supabase.co",
+        protocol: "https",
+        port: "",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
